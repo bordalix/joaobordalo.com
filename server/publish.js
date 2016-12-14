52afde72cac378509a141f5d855b77c6d651a1d2
João Bordalo <joao.bordalo@gmail.com>
2016-12-14 16:03:37 +0000

    ensure index exists on mongodb

--- server/publish.js
+++ server/publish.js
@@ -1,9 +1,17 @@
 Meteor.publish('posts', function(limit) {
-  return Posts.find({}, { sort: { id:-1 }, limit: limit });
+  return Posts.find({}, { sort: { id: -1 }, limit: limit });
+});
+
+Meteor.startup(function () {  
+  Posts._ensureIndex({id: 1});
 });
 
 Meteor.publish('thisPost', function(permalink) {
   return Posts.find({permalink: permalink});
+});
+
+Meteor.startup(function () {  
+  Posts._ensureIndex({permalink: 1});
 });
 
 Meteor.publish('allPosts', function(limit) {
