2eda9dcadd17fef87834eaf989732091491a98ee
João Bordalo <joao.bordalo@gmail.com>
2017-01-31 12:19:23 +0000

    adds https://osvaldas.info/auto-hide-sticky-header

--- client/layouts/layout.js
+++ client/layouts/layout.js
@@ -1,11 +1,35 @@
-var layoutMouseUpHandler = function(e) {
-    console.log('window.mouseup');
+// via https://osvaldas.info/auto-hide-sticky-header
+var layoutScrollHandler = function(e) {
+  if (typeof wScrollBefore === 'undefined') wScrollBefore  = 0;
+  let element	       = document.querySelector('.header');
+  let elHeight       = element.offsetHeight;
+  let dHeight			   = document.body.offsetHeight;
+  let wHeight			   = window.innerHeight;
+  let wScrollCurrent = window.pageYOffset;
+  let wScrollDiff    = wScrollBefore - wScrollCurrent;
+  let elTop          = parseInt(window.getComputedStyle(element).getPropertyValue('top')) + wScrollDiff;
+  if (wScrollCurrent <= 0) { // scrolled to the very top; element sticks to the top
+    element.style.top = '0px';
+  } else {
+    if (wScrollDiff > 0) { // scrolled up; element slides in
+      element.style.top = ( elTop > 0 ? 0 : elTop ) + 'px';
+    } else {
+      if (wScrollDiff < 0) { // scrolled down
+        if (wScrollCurrent + wHeight >= dHeight - elHeight) { // scrolled to the very bottom; element slides in
+          element.style.top = ( ( elTop = wScrollCurrent + wHeight - dHeight ) < 0 ? elTop : 0 ) + 'px';
+        } else { // scrolled down; element slides out
+          element.style.top = ( Math.abs( elTop ) > elHeight ? -elHeight : elTop ) + 'px';
+        }
+      }
+    }
+  }
+  wScrollBefore = wScrollCurrent;
 };
 
 Template.layout.onCreated(function() {
-    $(window).on('scroll', layoutMouseUpHandler);
+  $(window).on('scroll', layoutScrollHandler);
 });
 
 Template.layout.onDestroyed(function() {
-    $(window).off('scroll', layoutMouseUpHandler);
+  $(window).off('scroll', layoutScrollHandler);
 });
