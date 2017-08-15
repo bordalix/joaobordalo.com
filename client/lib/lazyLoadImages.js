dd24d7e3eab56cf9acad475cbb7b8f7e093e4778
João Bordalo <joao.bordalo@gmail.com>
2017-08-15 18:06:58 +0100

    :pencil: minor changes

--- client/lib/lazyLoadImages.js
+++ client/lib/lazyLoadImages.js
@@ -1,7 +1,5 @@
 lazyLoadImages = function () {
-  console.log('xpto');
   document.querySelectorAll('img[data-src]').forEach(function (img) {
-    console.log('img ', img);
     img.setAttribute('src', img.getAttribute('data-src'));
     img.onload = function() {  // eslint-disable-line no-param-reassign
       img.removeAttribute('data-src');
